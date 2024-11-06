38a205d9c4b56541893de8a89d6bfdb52738af12
LiMoN4iK6 <ksenyakochetkova2001@mail.ru>
2024-11-06 20:19:01 +0300

    feat: added routig for Read* page

--- frontend/src/layouts/MainLayout/MainLAyout.tsx
+++ frontend/src/layouts/MainLayout/MainLAyout.tsx
@@ -2,13 +2,19 @@ import React from 'react'
 import Header from './components/Header/Header'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from '../../pages/HomePage/HomePage'
+import LastReadPage from '../../pages/LastReadPage/LastReadPage'
+import WantReadPage from '../../pages/WantReadPage/WantReadPage'
+import ReadNowPage from '../../pages/ReadNowPage/ReadNowPage'
 
 const MainLayout: React.FC = () => {
   return (
     <div>
       <Header />
       <Routes>
-        <Route path="/home" element={<HomePage />} />
+        <Route path="/main" element={<HomePage />} />
+        <Route path="/lastRead" element={<LastReadPage />} />
+        <Route path="/wantRead" element={<WantReadPage />} />
+        <Route path="/readNow" element={<ReadNowPage />} />
       </Routes>
     </div>
   )
---
+++ frontend/src/pages/ReadNowPage/ReadNowPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ReadNowPage: React.FC = () => {
+  return <div>ReadNowPage</div>
+}
+
+export default ReadNowPage
