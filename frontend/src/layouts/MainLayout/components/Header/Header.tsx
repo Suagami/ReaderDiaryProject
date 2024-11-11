b8433b568a5386905278f0d338a4935eb031b28d
Shiroi228 <109740075+Shiroi228@users.noreply.github.com>
2024-11-11 15:04:22 +0300

    feat: added header

--- frontend/src/layouts/MainLayout/components/Header/Header.tsx
+++ frontend/src/layouts/MainLayout/components/Header/Header.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
 
+import s from './Header.module.css'
+
 const Header: React.FC = () => {
-  return <div>Header</div>
+  return (
+    <div className={s.headerWrapper}>
+      <button className={s.leftHeaderItems}>
+        <img src="../../../../../public/imges/pen.png" alt="pen.png" />
+        <div>Книжная полка</div>
+      </button>
+      <div className={s.rightHeaderItems}>
+        <button>Прочитанное</button>
+        <button>Хочу прочитать</button>
+        <button>Читаю сейчас</button>
+        <button className={s.profileButton} >
+          <img src="../../../../../public/imges/logo4.png" alt="logo4.png" />
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default Header
