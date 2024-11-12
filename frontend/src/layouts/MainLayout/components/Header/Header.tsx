94cb852f591582e057d1fa6fc84b050f7f7f3725
Shiroi228 <109740075+Shiroi228@users.noreply.github.com>
2024-11-12 22:06:40 +0300

    hotfix: resolved threads

--- frontend/src/layouts/MainLayout/components/Header/Header.tsx
+++ frontend/src/layouts/MainLayout/components/Header/Header.tsx
@@ -1,18 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import s from './Header.module.css'
 
 const Header: React.FC = () => {
   return (
     <div className={s.headerWrapper}>
-      <button className={s.leftHeaderItems}>
-        <img src="../../../../../public/imges/pen.png" alt="pen.png" />
-        <div>Книжная полка</div>
-      </button>
+      <Link to="/">
+        <div className={s.leftHeaderItems}>
+          <img src="../../../../../public/imges/pen.png" alt="pen.png" />
+          <div>Книжная полка</div>
+        </div>
+      </Link>
       <div className={s.rightHeaderItems}>
-        <button>Прочитанное</button>
-        <button>Хочу прочитать</button>
-        <button>Читаю сейчас</button>
+        <Link to="/lastRead">
+          <div className={s.rightHeaderItem}>Прочитанное</div>
+        </Link>
+        <Link to="/wantRead">
+          <div className={s.rightHeaderItem}>Хочу прочитать</div>
+        </Link>
+        <Link to="/readNow">
+          <div className={s.rightHeaderItem}>Читаю сейчас</div>
+        </Link>
         <button className={s.profileButton} >
           <img src="../../../../../public/imges/logo4.png" alt="logo4.png" />
         </button>
