07da9e2ad739450fb7f9a5581c619491eb5d9f0e
Shiroi228 <109740075+Shiroi228@users.noreply.github.com>
2024-12-09 22:02:15 +0300

    feat: added add book page

---
+++ frontend/src/pages/AddBookPage/AddBookPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import s from './AddBookPage.module.css'
+
+const AddBookPage: React.FC = () => {
+    return <div className={s.addBookPageWrapper}>
+        sdas
+    </div>
+}
+
+export default AddBookPage
